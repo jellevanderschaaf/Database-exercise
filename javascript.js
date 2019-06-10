2b0cc78a5b54d972df9c056d6d200ccfb46d8912
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-10 20:58:07 +0200

    Created database 'test', seems to have worked.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,34 @@
 const express = require('express');
 const mysql = require('mysql');
+
+
+const connection = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: 'jelle',
+
+});
+
+connection.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected');
+});
+
+const app = express();
+
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE databasetest';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('database created');
+    });
+});
+
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});
