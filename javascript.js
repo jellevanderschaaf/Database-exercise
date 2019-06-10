33416db43870ebc2674e72e374973a030ad75a24
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-10 22:28:35 +0200

    Created a table, fixed some errors (removed password, added database name.

--- javascript.js
+++ javascript.js
@@ -2,15 +2,16 @@ const express = require('express');
 const mysql = require('mysql');
 
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
 
     host: 'localhost',
     user: 'root',
-    password: 'jelle',
+    password: '',
+    database: 'databasetest',
 
 });
 
-connection.connect((err) => {
+db.connect((err) => {
     if (err) {
         throw err;
     }
@@ -20,12 +21,22 @@ connection.connect((err) => {
 const app = express();
 
 app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE databasetest';
+    let sql = 'CREATE DATABASE IF NOT EXISTS databasetest';
     db.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result);
         res.send('database created');
     });
+});
+
+
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Posts table created');
+    })
 });
 
 
