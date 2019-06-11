0de9482aa69d1c35ea08577ebc861237a956b86c
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-11 11:47:01 +0200

    Created a new database, new table and first entry.

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'databasetest',
+    database: 'diet_tracker',
 
 });
 
@@ -21,7 +21,7 @@ db.connect((err) => {
 const app = express();
 
 app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE IF NOT EXISTS databasetest';
+    let sql = 'CREATE DATABASE diet_tracker';
     db.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result);
@@ -29,16 +29,24 @@ app.get('/createdb', (req, res) => {
     });
 });
 
-
-app.get('/createpoststable', (req, res) => {
-    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+app.get('/createfooditemstable', (req, res) => {
+    let sql = 'CREATE TABLE food_items (id int AUTO_INCREMENT, name VARCHAR(255), fat VARCHAR(255), carbs VARCHAR(255), protein VARCHAR(255), kcals VARCHAR(255), price VARCHAR(255), PRIMARY KEY (id))';
     db.query(sql, (err, result) => {
         if (err) throw err;
         console.log(result);
-        res.send('Posts table created');
+        res.send('Food items table created');
     })
 });
 
+app.get('/addfooditem', (req, res) => {
+    let post = { name: 'kefir', fat: '1.6', carbs: '5', protein: '3.6', kcals: '48', price: '1' };
+    let sql = 'INSERT INTO food_items SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Food item added');
+    });
+});
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
